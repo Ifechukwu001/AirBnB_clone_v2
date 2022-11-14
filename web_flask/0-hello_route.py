101ee423f93ab600addcfe7681935d12f0ef1f16
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-11-14 07:41:01 +0100

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ Responds to the home route
@@ -17,6 +18,7 @@ def hello():
         str: Greeting.
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
