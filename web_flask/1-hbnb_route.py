095e0bb4c65eefc98a4b316e1015fabb77d4ad25
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-11-14 07:44:29 +0100

    added hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Flask Application
+
+    Attributes:
+        app (:obj:`Flask`): Instance of Flask.
+
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Responds to the home route
+
+    Return:
+        str: Greeting.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    """ Responds to the hbnb route
+
+    Return:
+        str: Greeting.
+    """
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
